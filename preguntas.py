247238044c7b4e8f5b1f6bd79b93ebf3b92323e0
Juan Jose Sapuyes <jsapuyesp@gmail.com>
2022-06-24 22:41:25 -0500

    Changes

--- preguntas.py
+++ preguntas.py
@@ -64,7 +64,7 @@ def pregunta_01():
     En esta función se realiza la carga de datos.
     """
     # Lea el archivo `mushrooms.csv` y asignelo al DataFrame `df`
-    df = df.read_csv('mushrooms.csv')
+    df = pd.read_csv('mushrooms.csv')
 
     # Remueva la columna `veil-type` del DataFrame `df`.
     # Esta columna tiene un valor constante y no sirve para la detección de hongos.
